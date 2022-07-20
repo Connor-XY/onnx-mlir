6c40ad2b54e2cc37269de358ec89774c22ed6c8c
Yan Xu <yan.xu@bytedance.com>
2022-07-20 18:04:32 -0400

    [Mhlo] add pow
    Signed-off-by: Yan Xu <yan.xu@bytedance.com>

--- src/Conversion/ONNXToMhlo/Math/Elementwise.cpp
+++ src/Conversion/ONNXToMhlo/Math/Elementwise.cpp
@@ -228,6 +228,34 @@ struct ONNXElementwiseBinaryOpLoweringToMhlo : public ConversionPattern {
   }
 };
 
+template <>
+struct ONNXElementwiseBinaryOpLoweringToMhlo<ONNXPowOp>
+    : public ConversionPattern {
+  ONNXElementwiseBinaryOpLoweringToMhlo(MLIRContext *ctx)
+      : ConversionPattern(ONNXPowOp::getOperationName(), 1, ctx) {}
+  LogicalResult matchAndRewrite(Operation *op, ArrayRef<Value> operands,
+      ConversionPatternRewriter &rewriter) const final {
+    Location loc = NameLoc::get(
+        StringAttr::get(op->getContext(), ONNXPowOp::getOperationName()),
+        op->getLoc());
+
+    ONNXGenericOpBroadcastedShapeHelper shapeHelper(op);
+    DimsExpr empty;
+    LogicalResult shapecomputed = shapeHelper.computeShape(operands, empty);
+    assert(succeeded(shapecomputed) && "Could not compute output shape");
+
+    int64_t outputRank = shapeHelper.outputRank;
+    llvm::SmallVector<Value, 4> broadcastedOperands =
+        getBroadcastedOperands(op, rewriter, loc, outputRank);
+    Value broadcastedLHS = broadcastedOperands[0];
+    Value broadcastedRHS = broadcastedOperands[1];
+    Value mhloOp = rewriter.create<mhlo::PowOp>(
+        loc, *op->result_type_begin(), broadcastedLHS, broadcastedRHS);
+    rewriter.replaceOp(op, mhloOp);
+    return success();
+  }
+};
+
 // Element-wise variadic ops lowering to Mhlo dialect.
 //===----------------------------------------------------------------------===//
 template <typename ElementwiseVariadicOp>
@@ -271,6 +299,7 @@ void populateLoweringONNXElementwiseOpToMhloPattern(
       ONNXElementwiseBinaryOpLoweringToMhlo<ONNXGreaterOrEqualOp>,
       ONNXElementwiseBinaryOpLoweringToMhlo<ONNXLessOp>,
       ONNXElementwiseBinaryOpLoweringToMhlo<ONNXLessOrEqualOp>,
+      ONNXElementwiseBinaryOpLoweringToMhlo<ONNXPowOp>,
       ONNXElementwiseVariadicOpLoweringToMhlo<ONNXSubOp>,
       ONNXElementwiseUnaryOpLoweringToMhlo<ONNXExpOp>,
       ONNXElementwiseUnaryOpLoweringToMhlo<ONNXSigmoidOp>,
