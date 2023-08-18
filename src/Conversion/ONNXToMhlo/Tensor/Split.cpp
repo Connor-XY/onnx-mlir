57da03b901be04a47afebeac51d508c55c8e2fd6
Yan Xu <yan.xu0210@bytedance.com>
2023-08-18 14:55:16 -0400

    [WIP][mhlo] add dynamic split

--- src/Conversion/ONNXToMhlo/Tensor/Split.cpp
+++ src/Conversion/ONNXToMhlo/Tensor/Split.cpp
@@ -60,44 +60,77 @@ struct ONNXSplitOpLoweringToMhlo : public ConversionPattern {
     } else if (split.getType().template isa<NoneType>()) {
       assert(!ShapedType::isDynamic(inputDimSize) &&
              "input dim size can't be dynamic");
-      int64_t sliceSize = inputDimSize / outputNum;
-      for (unsigned i = 0; i < outputNum; ++i)
-        splitSizes.push_back(sliceSize);
+      int64_t floorSliceSize = inputDimSize / outputNum;
+      unsigned numBiggerChunks = inputDimSize % outputNum;
+      int64_t ceilSliceSize = floorSliceSize + (numBiggerChunks ? 1 : 0);
+      for (unsigned int i = 0; i < numOfResults; ++i) {
+        if (i < numBiggerChunks)
+          splitSizes.push_back(ceilSliceSize);
+        else
+          splitSizes.push_back(floorSliceSize);
+      }
     } else {
       assert(false && "dynamic split not yet supported");
     }
 
-    SmallVector<int64_t, 4> sliceShape =
-        llvm::to_vector<4>(inputType.getShape());
-    SmallVector<int64_t, 4> beginIndices(rank, 0);
-    SmallVector<int64_t, 4> endIndices =
-        llvm::to_vector<4>(inputType.getShape());
-    SmallVector<int64_t, 4> strides(rank, 1);
     SmallVector<Value, 4> slices;
     slices.reserve(outputNum);
     int64_t beginIndice = 0;
     int64_t endIndice = 0;
+
+    if (inputType.hasStaticShape()) {
+      SmallVector<int64_t, 4> sliceShape =
+        llvm::to_vector<4>(inputType.getShape());
+      SmallVector<int64_t, 4> beginIndices(rank, 0);
+      SmallVector<int64_t, 4> endIndices =
+          llvm::to_vector<4>(inputType.getShape());
+      SmallVector<int64_t, 4> strides(rank, 1);
+
+      for (uint64_t i = 0; i < outputNum; ++i) {
+        sliceShape[dimIndex] = splitSizes[i];
+        Type sliceType =
+            RankedTensorType::get(sliceShape, inputType.getElementType());
+        endIndice += splitSizes[i];
+        beginIndices[dimIndex] = beginIndice;
+        endIndices[dimIndex] = endIndice;
+        slices.push_back(rewriter.create<mhlo::SliceOp>(loc, sliceType, input,
+            DenseIntElementsAttr::get(
+                RankedTensorType::get(
+                    {static_cast<int64_t>(beginIndices.size())}, indiceType),
+                beginIndices),
+            DenseIntElementsAttr::get(
+                RankedTensorType::get(
+                    {static_cast<int64_t>(endIndices.size())}, indiceType),
+                endIndices),
+            DenseIntElementsAttr::get(
+                RankedTensorType::get(
+                    {static_cast<int64_t>(strides.size())}, indiceType),
+                strides)));
+        beginIndice = endIndice;
+      }
+      rewriter.replaceOp(op, slices);
+      return success();
+    } 
+    
+    Value zero = rewriter.create<mhlo::ConstantOp>(loc,
+        DenseIntElementsAttr::get(RankedTensorType::get({1}, indiceType),
+            ArrayRef<int64_t>{0}));
+    Value one = rewriter.create<mhlo::ConstantOp>(loc,
+        DenseIntElementsAttr::get(RankedTensorType::get({1}, indiceType),
+            ArrayRef<int64_t>{1}));
+    SmallVector<Value, 4> stepValues;
+    SmallVector<Value, 4> beginValues;
+    SmallVector<Value, 4> endValues;
+    for (int64_t i = 0; i < rank; ++i) {
+      beginValues.push_back(zero);
+      stepValues.push_back(one);
+      if ()
+      endValues.push_back();
+    }
     for (uint64_t i = 0; i < outputNum; ++i) {
-      sliceShape[dimIndex] = splitSizes[i];
-      Type sliceType =
-          RankedTensorType::get(sliceShape, inputType.getElementType());
       endIndice += splitSizes[i];
-      beginIndices[dimIndex] = beginIndice;
-      endIndices[dimIndex] = endIndice;
-      slices.push_back(rewriter.create<mhlo::SliceOp>(loc, sliceType, input,
-          DenseIntElementsAttr::get(
-              RankedTensorType::get(
-                  {static_cast<int64_t>(beginIndices.size())}, indiceType),
-              beginIndices),
-          DenseIntElementsAttr::get(
-              RankedTensorType::get(
-                  {static_cast<int64_t>(endIndices.size())}, indiceType),
-              endIndices),
-          DenseIntElementsAttr::get(
-              RankedTensorType::get(
-                  {static_cast<int64_t>(strides.size())}, indiceType),
-              strides)));
-      beginIndice = endIndice;
+      slices.push_back(rewriter.create<mhlo::RealDynamicSliceOp>(loc, outputType, input,
+          start_indices, end_indices, step_indices));
     }
     rewriter.replaceOp(op, slices);
     return success();
